01396fcedec396180b713c10804d375261ef7ba4
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-06-09 15:08:44 -0600

    ci: :sparkles: rollback
    rollback
    
    rollback

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,12 +1,10 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
-import * as os from 'os';
 
 @Controller()
 export class AppController {
   @Get()
   getHello(@Res() res: Response): void {
-    const hostname = os.hostname(); // Obtiene el nombre del contenedor
-    res.status(200).send(`<h1> OK DESDE EL SERVIDOR ${hostname} </h1>`);
+    res.status(200).send(`<h1> OK DESDE EL SERVIDOR UNO </h1>`);
   }
 }
