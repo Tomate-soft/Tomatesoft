b1f82cc616c9be4b07e0d8c58151acd0f62c097e
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-06-09 15:07:44 -0600

    build: :sparkles: view instance name
    view instance name
    
    view instance name

--- server/src/app.controller.ts
+++ server/src/app.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Get, Res } from '@nestjs/common';
 import { Response } from 'express';
+import * as os from 'os';
 
 @Controller()
 export class AppController {
   @Get()
   getHello(@Res() res: Response): void {
-    res.status(200).send(`<h1> OK DESDE EL SERVIDOR UNO </h1>`);
+    const hostname = os.hostname(); // Obtiene el nombre del contenedor
+    res.status(200).send(`<h1> OK DESDE EL SERVIDOR ${hostname} </h1>`);
   }
 }
