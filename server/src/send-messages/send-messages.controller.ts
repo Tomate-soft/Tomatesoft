001af3427f41003b789b240c445ff791621a3499
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-07-04 15:43:36 -0600

    refactor: :memo: last changes hot reload
    last changes hot reload
    
    last changes hot reload

--- server/src/send-messages/send-messages.controller.ts
+++ server/src/send-messages/send-messages.controller.ts
@@ -1,4 +1,10 @@
-import { Controller, Post } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Body,
+  HttpException,
+  HttpStatus,
+} from '@nestjs/common';
 import { SendMessagesService } from './send-messages.service';
 
 @Controller('send-messages')
@@ -6,13 +12,16 @@ export class SendMessagesController {
   constructor(private sendMessageService: SendMessagesService) {}
 
   @Post()
-  async SendMessageController(body: { message: string }) {
-    {
-      try {
-        await this.sendMessageService.SendTelegramMessage(body.message);
-      } catch (error) {
-        console.error(error);
-      }
+  async sendMessageController(@Body() body: { message: string }) {
+    try {
+      await this.sendMessageService.SendTelegramMessage(body.message);
+      return { success: true, message: 'Mensaje enviado a Telegram' };
+    } catch (error) {
+      console.error(error);
+      throw new HttpException(
+        'Error enviando mensaje a Telegram',
+        HttpStatus.INTERNAL_SERVER_ERROR,
+      );
     }
   }
 }
