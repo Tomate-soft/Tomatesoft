41487dfb1d439c51d9440b694a7599fcda27ea87
Moises Baldenegro Melendez <mc.moisesm16@gmail.com>
2025-07-07 11:19:47 -0600

    build: :sparkles: va de nuez
    va de nuez
    
    va de nuez

--- server/src/ventas/cancellations/cancellations.service.ts
+++ server/src/ventas/cancellations/cancellations.service.ts
@@ -86,7 +86,7 @@ export class CancellationsService {
           { status: FREE_STATUS, bill: [] },
           { new: true },
         );
-        const message = `${newCancellation}`;
+        const message = `${newCancellation.populate({ path: 'accountId' })}`;
         await this.sendMessagesService.SendTelegramMessage(
           message,
           -1002859358686,
